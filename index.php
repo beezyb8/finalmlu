2e0c37f31d204d64f3e96cea263122356b8cdfa9
beezyb8 <beharb@umich.edu>
2022-11-08 14:27:11 -0500

    Login form and CSS for login form

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/login_reg.css">
+</head>
+<body>
+    <form class="entire-bin">
+        <div class="bin-bin-bin">
+            <div class="login-password-bin">
+                <label class="login-password-header" for="form-stacked-text">Email</label>
+                <div class="login-password-input-bin">
+                    <input class="email-bin" id="form-stacked-text" type="email" placeholder="example@umich.edu" required="required">
+                </div>
+            </div>
+
+            <div class="login-password-bin">
+                <label class="login-password-header" for="form-stacked-select">Password</label>
+                <div class="login-password-input-bin">
+                    <input class="password-bin" id="form-stacked-text" type="password" placeholder="password" required="required">
+                </div>
+            </div>
+
+            <div class="submit-bin">
+                <button class="submit-button">Login</button>
+            </div>
+        </div>
+    </form>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.15.12/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.15.12/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
