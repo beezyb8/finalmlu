da98f72ac123bd2dd87dccee8695a52338753746
beezyb8 <beharb@umich.edu>
2022-11-08 15:16:22 -0500

    Further update 11.8

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php 
+    define('__CONFIG__', true);
+    require_once "inc/config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/login_reg.css">
+</head>
+<body>
+    <form class="entire-bin">
+        <div class="bin-bin-bin">
+            <div class="login-password-bin">
+                <label class="login-password-header" for="form-stacked-text">Email</label>
+                <div class="login-password-input-bin">
+                    <input class="email-bin" id="form-stacked-text" type="email" placeholder="example@umich.edu" required="required">
+                </div>
+            </div>
+
+            <div class="login-password-bin">
+                <label class="login-password-header" for="form-stacked-select">Password</label>
+                <div class="login-password-input-bin">
+                    <input class="password-bin" id="form-stacked-text" type="password" placeholder="password" required="required">
+                </div>
+            </div>
+
+            <div class="submit-bin">
+                <button class="submit-button">Login</button>
+            </div>
+        </div>
+    </form>
+
+    <?php require_once "inc/footer.php"; ?>
+</body>
+</html>
