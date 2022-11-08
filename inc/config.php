50b582547f52f4888018367820201d32b6bff3fa
beezyb8 <beharb@umich.edu>
2022-11-08 15:16:03 -0500

    Further update 11.8

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php 
+    if(!defined('__CONFIG__')) {
+        exit('404 Gateway Error, Text 1(516)640-2417 and inform about error');
+    }
+
+    define('__ALLOWFOOTER__', true);
+?>
---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+<?php 
+    if(!defined('__ALLOWFOOTER__')) {
+        exit('404 Gateway Error, Text 1(516)640-2417 and inform about error');
+    }
+?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.12/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.12/dist/js/uikit-icons.min.js"></script>
